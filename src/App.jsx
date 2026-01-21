0d159d4173525b81910b7f0f86b2770ad01cbb65
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-21 22:20:42 +0100

    tog bort onödig kodrad

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <div className='body-container'>
       <Header changePage={setActivePage}/>
       <main>
-        {pages[activePage] || <Home />}
+        {pages[activePage]}
       </main>
     </div>
   )
