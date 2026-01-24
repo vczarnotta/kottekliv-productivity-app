3916b175c0ec73e5396200f296395d1aec7415cd
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-24 20:39:39 +0100

    tömde dashboard sidan

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,15 +3,7 @@ import Card from "../components/Card/Card"
 function Dashboard() {
   return(
     <div className="main-container">
-        <Card 
-          // Lägg koppling till antalet tasks här
-          title={"0/0"}
-          description={"Uppgifter"}
-        />
-        <Card 
-          title={"Titel"}
-          description={"Beskrivning"}
-        />
+
     </div>
   )
 }
