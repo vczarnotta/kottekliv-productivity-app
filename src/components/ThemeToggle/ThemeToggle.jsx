08039b796b40540df4aeb0c696d64fbf16fbc053
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-25 11:44:20 +0100

    theme sparas i localStorage

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -5,12 +5,19 @@ import Button from "../Button/Button"
 import { CiDark, CiLight } from "react-icons/ci";
 
 function ThemeToggle() {
-  const [ isLight, setIsLight ] = useState(true)
+  // Om värde finns i localStorage, returnera från sträng till true/false annars true
+  const [ isLight, setIsLight ] = useState(() =>{
+    const savedTheme = localStorage.getItem("theme")
+    return savedTheme === null ? true : savedTheme === "true"
+  })
   
   //Lägger till och tar bort klassen dark från #root när state ändras
   useEffect(() => {
     const root = document.querySelector("#root")
     isLight ? root.classList.remove("dark") : root.classList.add("dark")
+
+    // Spara värdet i localstorage som sträng
+    localStorage.setItem("theme", isLight);
   }, [isLight])
 
   // Ändrar state när knappen trycks
