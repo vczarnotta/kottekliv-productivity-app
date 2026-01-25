aeb1ee662676c80919671640335facdb7664aa8e
David Lindblom <lindblom.david@hotmail.com>
2026-01-25 22:52:51 +0100

    set up boilerplate for timer context

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { TodoProvider } from './context/TodoContext.jsx'
+import { TimerProvider } from './context/TimerContext.jsx'
 
 // TodoProvider gör så att den går att använda hela vägen neråt i DOM trädet. det blir som en antenn
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
+    <TimerProvider>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </TimerProvider>
   </StrictMode>,
 )
