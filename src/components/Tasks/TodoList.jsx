e25d41f9e3e0eab73615c0b2ed763ba813c85649
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-25 11:56:07 +0100

    matchade Button props i Todo för uppd vers.

--- src/components/Tasks/TodoList.jsx
+++ src/components/Tasks/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState, useEffect, useCallback, useContext } from "react";
+import { useState, useContext } from "react";
 import Button from "../Button/Button";
 import "./TodoList.css";
 import { TodoContext } from "../../context/TodoContext";
@@ -6,47 +6,53 @@ import { TodoContext } from "../../context/TodoContext";
 
 function TodoList() {
 
-    const [text, setText] = useState("");
-    const {state, dispatch, totalItems} = useContext(TodoContext);
+  const [text, setText] = useState("");
+  const {state, dispatch, totalItems} = useContext(TodoContext);
 
 
-    const handleSubmit = (event) => { // avoids weird refresh i think?
-        event.preventDefault();
-        if (text.trim() === "") return;
+  const handleSubmit = (event) => { // avoids weird refresh i think?
+      event.preventDefault();
+      if (text.trim() === "") return;
 
-        // sends text as payload and then deletes it from text useState
-        dispatch({ type: "ADD", payload: text}) 
-        setText("");
-    };
+      // sends text as payload and then deletes it from text useState
+      dispatch({ type: "ADD", payload: text}) 
+      setText("");
+  };
 
 
-    return (
-    <>
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="text"
-            value={text}
-            onChange={(event => setText(event.target.value))}
-            placeholder="Lägg til en grejs" 
-            /> 
-            <Button type="submit" onClick={() => {console.log()}}>Lägg till</Button>
-        </form>
-        <h3>{totalItems} saved items on the list</h3>
-        <ul>
-            {state.map((task) => (
-                <li key={task.id}>
-                    {task.text}
-                    <button onClick={() => dispatch({ type: "DELETE", payload: task.id})}>
-                        X
-                    </button>
-                </li>
-            ))}
-        </ul>
+  return (
+  <>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        value={text}
+        onChange={(event => setText(event.target.value))}
+        placeholder="Lägg til en grejs" 
+      /> 
+      <Button 
+        type="submit"
+        onClick={() => {console.log()}}
+        label={"Lägg till"} 
+      />
+    </form>
 
-        {/* bugfix // dev */}
-        <button onClick={() => console.log(localStorage.getItem("users-todo-list"))}>console log</button>
-    </>
-    )
+    <h3>{totalItems} saved items on the list</h3>
+
+    <ul>
+      {state.map((task) => (
+        <li key={task.id}>
+          {task.text}
+          <button onClick={() => dispatch({ type: "DELETE", payload: task.id})}>
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+
+    {/* bugfix // dev */}
+    <button onClick={() => console.log(localStorage.getItem("users-todo-list"))}>console log</button>
+  </>
+  )
 }
 
 export default TodoList
