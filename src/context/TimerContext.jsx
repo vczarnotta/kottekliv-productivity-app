264a58134ce1491f9670417e37d0301419a01781
David Lindblom <lindblom.david@hotmail.com>
2026-01-27 18:47:23 +0100

    added save inside usereducer

--- src/context/TimerContext.jsx
+++ src/context/TimerContext.jsx
@@ -27,15 +27,50 @@ import { createContext, useReducer, useRef } from "react";
 // Storage -> useReducer()
 // Memory -> Localstorage
 
-// current session + all sessions
-let initialstate = {
-  isRunning: false,
-  msPassed: 0,
-  sessions: [], // saves session object. {id, startTime, endTime, sessionLengthMs}  (later i can use sessions.startTime in a filter/function to sort what days it happened)
+// // current session + all sessions -> grab from localstorage in the future
+// let initialstate = {
+//   isRunning: false,
+//   msDuration: 0,
+//   msStartTime: 0,
+//   msEndTime: 0,
+//   sessions: [], // saves session object. {id, startTime, endTime, msDuration}  (later i can use sessions.startTime in a filter/function to sort what days it happened)
+// }
+
+// get saved list from previous session, or return empty list
+const getSavedTimers = () => {
+  const savedData = localStorage.getItem("user-timers")
+
+  return savedData ? JSON.parse(savedData) : [];
+
+// typeof(localStorage.getItem("users-todo-list")) ----> string ----> empty string == falsy
+};
+
+
+// useReducer (how i interact with the timer and save it)
+const timerReducer = (state, action) => {
+  switch (action.type) {
+    case "SAVE":
+      // saving format
+      const newSession = {
+        id: crypto.randomUUID(),
+        msDuration: state.msDuration,
+        msStartTime: state.msStartTime,
+        msEndTime: state.msEndTime,
+      }
+      // resets and saves
+      return {
+        ...state,
+        isRunning: false,
+        msDuration: 0,
+        msStartTime: 0,
+        msEndTime: 0,
+        sessions: [...state.sessions, newSession]
+      };
+    
+    default:
+      return state;
+  }
 }
-
-
-
 
 
 
