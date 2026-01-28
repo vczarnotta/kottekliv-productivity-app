6a63761f08fb4a34f9e63b8d7c903ce396a68472
David Lindblom <lindblom.david@hotmail.com>
2026-01-28 22:19:01 +0100

    added console log button to see history

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -4,18 +4,19 @@ import Button from "../Button/Button";
 import styles from "./Timer.module.css";
 
 function Timer() {
-  const {currentTimer, start, pause, save } = useContext(TimerContext);
+  const {currentTimer, start, pause, save, state } = useContext(TimerContext);
 
 
   return (
     <>
       <h1>Timer:</h1>
       <h2>{currentTimer()}</h2>
-      
+      <h3>{state.sessions[0].msTotalWorked}</h3>
       <div className={styles.buttons}>
         <Button size="small" onClick={start}>Start</Button>
         <Button size="small" onClick={pause}>Pause</Button>
         <Button size="small" onClick={save}>Save</Button>
+        <button style={{color: "black"}} onClick={() => {console.log(state.sessions)}}>console log history</button>
       </div>
       <h3>Timer History:</h3>
     </>
