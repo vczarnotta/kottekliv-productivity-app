e991c3d3b0a3f18dea6d941a9dd637c7c4ba8e3d
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-28 15:49:06 +0100

    skapat css och grunden för historikmodalen

--- src/components/Modal/ShowHistorySessionModal/showHistorySessionModal.jsx
+++ src/components/Modal/ShowHistorySessionModal/showHistorySessionModal.jsx
@@ -1,9 +1,62 @@
 import Modal from "../Modal";
+import mockSessions from "./mockSessions";
+import Card from "../../Card/Card"
+import Button from "../../Button/Button";
+import "./ShowHistorySessionModal.css"
 
 function ShowHistorySessionModal({onClose}) {
+  const editSession = () => {
+
+  }
+
+  const deleteSession = () => {
+
+  }
+
   return(
     <Modal onClose={onClose}>
+      <h2>Session History</h2>
       
+      
+      <div className="session-list">
+        {mockSessions.map((session) => (
+          <div>
+            <Card key={session.id}>
+              <div className="content">
+
+                <h3>{session.sessionName || "Untitled Session"}</h3>
+
+                <div>
+                  <p className="category-text">{session.category}</p>
+                  <p className="performance-text">Performance - {session.performance.split('-')[1]}</p>
+                </div>
+
+                <p className="time-info">
+                  <span>{session.date}</span>
+                  <span className="separator">•</span>
+                  <span>{session.startTime} - {session.endTime}</span>
+                </p>
+
+                <div className="button-row">
+                  <Button
+                    label="Edit"
+                    onClick={editSession}
+                    size="small"
+                  />
+
+                  <Button
+                    label="Delete"
+                    onClick={deleteSession}
+                    variant="secondary"
+                    size="small"
+                  />
+                </div>
+              </div>
+            </Card>
+
+          </div>
+        ))}
+      </div>
     </Modal>
   )
 }
