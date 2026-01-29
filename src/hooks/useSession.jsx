dc76074d0a244018f5387aef7e7aa1dfbd481cfb
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-29 16:50:40 +0100

    skapat sessionContext

---
+++ src/hooks/useSession.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import SessionContext from "../context/Session/SessionContext"
+
+function useSessions() {
+  const context = useContext(SessionContext);
+  if (!context) throw new Error("useSessions must be used within a SessionProvider");
+  return context;
+}
+
+export default useSessions
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { TodoProvider } from './context/TodoContext.jsx'
 import { TimerProvider } from './context/TimerContext.jsx'
+import SessionProvider from './context/Session/SessionProvider.jsx'
 
 // TodoProvider gör så att den går att använda hela vägen neråt i DOM trädet. det blir som en antenn
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TimerProvider>
-      <TodoProvider>
-        <App />
-      </TodoProvider>
-    </TimerProvider>
+    <SessionProvider>
+      <TimerProvider>
+        <TodoProvider>
+          <App />
+        </TodoProvider>
+      </TimerProvider>
+    </SessionProvider>
   </StrictMode>,
 )
