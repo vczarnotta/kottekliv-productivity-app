1ca3a02d59f4f729dc2ad64ead12035167590d03
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-02-12 11:46:59 +0100

    quote från api klart

---
+++ src/components/Quote/Quote.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import "./Quote.css"
+
+function Quote() {
+  const [ quote, setQuote ] = useState("")
+  
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const url = "https://api.adviceslip.com/advice"
+  
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        const adviceQuote = data.slip
+
+
+        setQuote(adviceQuote.advice)
+
+      } catch (error) {
+        console.error(`Error getting quote: ${error}`)
+      }
+    }
+
+    fetchQuote()
+  }, [])
+
+  return(
+    <div className="quote-container">
+      {quote ? (
+        <p className="quote">{quote}</p>
+      ) : (
+        <p className="quote loading-text">
+          Loading quote
+          <span className="dots">
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </span>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Quote
