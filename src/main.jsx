680011dda23e9f743aadda4361119ff9a65289b6
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-02-17 09:41:14 +0100

    la ihop allt med sessioncontext i en fil

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { TodoProvider } from './context/TodoContext.jsx'
 import { TimerProvider } from './context/TimerContext.jsx'
-import SessionProvider from './context/Session/SessionProvider.jsx'
+import SessionProvider from './context/SessionProvider.js'
 
 // TodoProvider gör så att den går att använda hela vägen neråt i DOM trädet. det blir som en antenn
 
