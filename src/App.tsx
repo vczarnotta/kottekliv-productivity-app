e68a63341acca05bfd487f83ee8f1b84cc162a67
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-02-17 17:10:03 +0100

    ändrade till routing och tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import HomePage from './pages/HomePage'
+import TaskPage from  "./pages/TaskPage"
+import TimerPage from "./pages/TimerPage/TimerPage"
+import HistoryPage from "./pages/HistoryPage"
+import './App.css'
+
+function App() {
+  return(
+    <div className='body-container'>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/Tasks' element={<TaskPage/>} />
+          <Route path='/Timer' element={<TimerPage/>} />
+          <Route path='/History' element={<HistoryPage/>} />
+          <Route path='*' element={"Page not found"} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
--- src/components/Header/navData.tsx
+++ src/components/Header/navData.tsx
@@ -1,20 +1,30 @@
 import { GoHome, GoCheckbox, GoClock, GoGraph } from "react-icons/go"
 
+interface menuItem {
+  title: string,
+  path: string,
+  icon: React.ReactNode
+}
+
 // Centralized list for easily adding or removing menu items
-const menuItems = [
+const menuItems: menuItem[] = [
   {title: "Home",
+    path: "/",
     icon: <GoHome />,
   },
 
-  {title: "Tasks", 
+  {title: "Tasks",
+    path: "/Tasks",
     icon: <GoCheckbox />,
   },
 
-  {title: "Timer", 
+  {title: "Timer",
+    path: "/Timer",
     icon: <GoClock />,
   },
 
   {title: "History",
+    path: "/History",
     icon: <GoGraph />,
   },
 ]
